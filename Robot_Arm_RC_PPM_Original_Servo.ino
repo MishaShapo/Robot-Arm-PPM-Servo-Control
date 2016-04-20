86e1b9f771f622d87287adaf47556a32cfe94eca
Michail Shaposhnikov <misha.shapo98@gmail.com>
2016-04-20 00:36:04 -0500

    Moderately working
    Dad helped and it is currently working ok. PPM signal seems to jitter
    way too much though. May just go back to potentiometers but we’ll see.

--- Robot_Arm_RC_PPM_Original_Servo.ino
+++ Robot_Arm_RC_PPM_Original_Servo.ino
@@ -16,6 +16,7 @@
 // rcarduino.blogspot.com
 // 
 #define SERVO_FRAME_SPACE 6
+#define BASE_VAL 1448
 /* 
  * 0 = Alieron 
    1 = Elevator
@@ -40,14 +41,17 @@ int numConnections = sizeof(channels)/sizeof(uint8_t) ;
 uint16_t input = 0;
 
 int prevPWM[] = {0,0,0,0,0,0};
-int curPWM[] = {1500,1500,1500,1500,1500,1500};
+int curPWM[] = {1500,1500,1500,BASE_VAL,1500,1500};
 int deltaPWM;
 
 
-int servoMin[] = {500,  2100,  750,  2500, 2500, 1307};
-int servoMax[] = {2500, 809, 2445, 500,  500, 2486};
+//int servoMin[] = {500,  2100,  750,  2500, 2500, 1307};
+//int servoMax[] = {2500, 809, 2445, 500,  500, 2486};
 
-int baseVal = 1448;
+int servoMin[] = {500,  500,  500,  BASE_VAL-200, 500, 500};
+int servoMax[] = {2500, 2500, 2500, BASE_VAL+200,  2500, 2500};
+
+
 
 
 
@@ -56,17 +60,17 @@ volatile uint32_t ulCounter = 0;
 void setup()
 {
   Serial.begin(115200);
-  Serial.println("Testing PPM.");
-  Serial.print("Number of Connections : ");
-  Serial.println(numConnections);
+//  Serial.println("Testing PPM.");
+//  Serial.print("Number of Connections : ");
+//  Serial.println(numConnections);
 
   // attach servo objects, these will generate the correct
   // pulses for driving Electronic speed controllers, servos or other devices
   // designed to interface directly with RC Receivers 
-
-  for(int i = 0; i < numConnections; i++){
-    servos[i].attach(servoPins[i],500,2500);
-  }
+//
+//  for(int i = 0; i < numConnections; i++){
+//    servos[i].attach(servoPins[i],500,2500);
+//  }
   CRCArduinoPPMChannels::begin();
 }
 
@@ -74,24 +78,26 @@ void loop()
 {
   // Pass the signals straight through - 
   for(int i = 0; i < numConnections; i++){
-    if(prevPWM[i] == 0){
-      prevPWM[i] = map(CRCArduinoPPMChannels::getChannel(channels[i]),1000,2000,500,2500);
-      continue;
-    } else if(CRCArduinoPPMChannels::getChannel(channels[i])) {
-      deltaPWM = map(CRCArduinoPPMChannels::getChannel(channels[i]),1000,2000,500,2500) - prevPWM[i];
-      curPWM[i] = (abs(deltaPWM) > 5) ? curPWM[i] + deltaPWM : (i == 3) ? baseVal : curPWM[i];
-      Serial.print("Sending PWM to servo with channel : ");
-      Serial.print(channels[i]);
-      Serial.print(" with PWM of : ");
-      Serial.println(curPWM[i]);
+    input = CRCArduinoPPMChannels::getChannel(channels[i]);
+    curPWM[i] = map(input,1000,2000,servoMin[i],servoMax[i]);
+    if(abs(curPWM[i] - prevPWM[i]) > 10){
       prevPWM[i] = curPWM[i];
-      servos[i].writeMicroseconds(curPWM[i]);
+//      Serial.print("curPWM[");
+//      Serial.print(channels[i]);
+//      Serial.print("] is : ");
+//      Serial.println(curPWM[i]);
+//      Serial.println(curPWM[i]);
+//      servos[i].writeMicroseconds(curPWM[i]);
+    }
+    
+    if(i == 2){
+      Serial.println(curPWM[i]);
     }
   }
-  if(CRCArduinoPPMChannels::getSynchErrorCounter()){
-    Serial.print("Synch Error Counter: ");
-    Serial.println(CRCArduinoPPMChannels::getSynchErrorCounter());
-  }
+//  if(CRCArduinoPPMChannels::getSynchErrorCounter()){
+//    Serial.print("Synch Error Counter: ");
+//    Serial.println(CRCArduinoPPMChannels::getSynchErrorCounter());
+//  }
   delay(50);
 }
 
